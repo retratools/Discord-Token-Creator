6b5b80a748a5acee1d8087ef2e8a0505dbee7ece
RetraTools <83399857+retratools@users.noreply.github.com>
2021-04-29 18:52:27 +0300

    Add files via upload

---
+++ Discord Token Creator/Discord Token Creator/Get/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Threading;
+
+namespace Discord_Token_Creator
+{
+    public class Settings
+    {
+        public static void AutoLoadSettings()
+        {
+            Console.Clear();
+            if (File.Exists("Settings.retra"))
+            {
+                string str = File.ReadAllText("Settings.retra");
+                try
+                {
+                    Variables.TwoCapKey = Functions.LR(str, "[2capkey]-", "-[2capkey]").FirstOrDefault();
+                }
+                catch
+                {
+                    Console.WriteLine("Error Parsing Configuration...");
+                    Thread.Sleep(200);
+                    File.Delete("Settings.retra");
+                    AutoLoadSettings();
+                }
+
+            }
+            else
+            {
+                Console.Write("2cap Key: ");
+                Variables.TwoCapKey = Console.ReadLine();
+                File.AppendAllText("Settings.retra", "[2capkey]-" + Variables.TwoCapKey + "-[2capkey]" + Environment.NewLine);
+
+
+            }
+        }
+    }
+}
